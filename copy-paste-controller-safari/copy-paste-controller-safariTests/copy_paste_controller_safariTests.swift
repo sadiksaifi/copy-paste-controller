e4ae21cdd3f65b1b0284e9e587e63dbccbb121a3
Sadik Saifi <email@sadiksaifi.dev>
2024-12-16 03:05:35 +0530

    docs: add license

---
+++ copy-paste-controller-safari/copy-paste-controller-safariTests/copy_paste_controller_safariTests.swift
@@ -0,0 +1,17 @@
+//
+//  copy_paste_controller_safariTests.swift
+//  copy-paste-controller-safariTests
+//
+//  Created by Sadik Saifi on 16/12/24.
+//
+
+import Testing
+@testable import copy_paste_controller_safari
+
+struct copy_paste_controller_safariTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ copy-paste-controller-safari/copy-paste-controller-safariUITests/copy_paste_controller_safariUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  copy_paste_controller_safariUITestsLaunchTests.swift
+//  copy-paste-controller-safariUITests
+//
+//  Created by Sadik Saifi on 16/12/24.
+//
+
+import XCTest
+
+final class copy_paste_controller_safariUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
